8bcea0b8c10b837f5241a55cb0a48ddf1129d4ea
Paco van der Linden <pavadeli@gmail.com>
2021-01-21 15:05:54 +0100

    tuned CHANGELOG

--- release.sh
+++ release.sh
@@ -8,6 +8,5 @@ npm run build
 # git push --follow-tags origin main
 
 npm publish dist/libs/sherlock --access public
-npm publish dist/libs/sherlock-proxy --access public
 npm publish dist/libs/sherlock-rxjs --access public
 npm publish dist/libs/sherlock-utils --access public
