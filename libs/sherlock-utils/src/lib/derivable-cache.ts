c164dd7e6dbd1c3413f9ec3f5a948176fc9c7da2
Paco van der Linden <pavadeli@gmail.com>
2021-01-29 11:17:05 +0100

    feat(sherlock-utils)!: improved/simplified derivableCache API

--- libs/sherlock-utils/src/lib/derivable-cache.ts
+++ libs/sherlock-utils/src/lib/derivable-cache.ts
@@ -1,80 +1,71 @@
 import {
     atom,
     Derivable,
-    derive,
-    isDerivable,
     isSettableDerivable,
     lens,
     LensDescriptor,
     SettableDerivable,
-    Unwrappable,
     _internal,
 } from '@skunkteam/sherlock';
 
-export interface MapImplementation<K, V> {
-    set(key: K, value: V): void;
-    delete(key: K): void;
-    get(key: K): V | undefined;
-}
+type DerivableFactory<I, V> = (input: I) => Derivable<V>;
 
-export interface DerivableCacheOptions<K, V> {
-    derivableFactory(key: K): Derivable<V>;
-    mapFactory?(): MapImplementation<K, Derivable<V>>;
+export interface DerivableCacheOptions<I, K = I> {
+    mapKeys?(key: I): K;
     delayedEviction?: boolean;
 }
 
-export type DerivableCache<K, V> = (key: Unwrappable<K>) => SettableDerivable<V>;
-
-function defaultMapFactory<K, V>(): MapImplementation<K, V> {
-    return new Map();
+export interface DerivableCache<I, V, K = I> {
+    (key: I): SettableDerivable<V>;
+    observerCount(): number;
+    connectedKeys(): IterableIterator<K>;
+    connectedKeyCount(): number;
 }
 
-const CACHED_PROXY = Symbol('cachedProxy');
+export function derivableCache<I, V, K = I>(
+    derivableFactory: DerivableFactory<I, V>,
+    { delayedEviction, mapKeys = v => (v as unknown) as K }: DerivableCacheOptions<I, K> = {},
+): DerivableCache<I, V, K> {
+    const cache = new Map<K, CacheItem<V>>();
 
-declare module '@skunkteam/sherlock' {
-    export interface Derivable<V> {
-        [CACHED_PROXY]?: SettableDerivable<V>;
-    }
-}
+    const injectedDependency = atom(null);
 
-const nonFinalAtom = atom(0);
-
-export function derivableCache<K, V>(opts: DerivableCacheOptions<K, V>): DerivableCache<K, V> {
-    const cache = (opts.mapFactory || defaultMapFactory)();
-
-    const { delayedEviction, derivableFactory } = opts;
-    const descriptor: LensDescriptor<V, [K]> = {
-        get(key) {
-            const cachedDerivable = cache.get(key);
-            // If the cache has a hit for the current key, we know it is already connected (through another proxy).
-            if (cachedDerivable) {
-                return cachedDerivable.getState();
-            }
-
-            // A cache miss means no other proxy is currently connected.
-            const newDerivable = _internal.independentTracking(() => derivableFactory(key));
+    const descriptor: LensDescriptor<V, [I, K]> = {
+        get(input, key) {
             // We don't want final-value-optimalization, because that defeats the purpose of the cache. A final value
             // is not registered as an observed value, which means we cannot track the usage of our newly created derivable.
-            // Therefore introduce a non-final atom (`atom(0)`) in the derivation:
-            const derivable = isSettableDerivable(newDerivable)
-                ? lens<V, [number]>({ get: () => newDerivable.get(), set: v => newDerivable.set(v) }, nonFinalAtom)
-                : derive(() => newDerivable.get(), nonFinalAtom);
+            // Therefore introduce a non-final atom (`atom(null)`) in the derivation:
+            injectedDependency.get();
 
-            if (delayedEviction) {
-                derivable.autoCache();
+            const cacheEntry = cache.get(key);
+            // If the cache has a hit for the current key, we know it is either already connected (through an "outer" proxy) or
+            // we want to connect after initializing the inner derivable.
+            if (cacheEntry) {
+                return cacheEntry.inner.getState();
             }
 
-            // Get the state of our derivable early so it connects when needed.
-            const state = derivable.getState();
-            if (derivable.connected) {
-                derivable[CACHED_PROXY] = this as SettableDerivable<V>;
-                cache.set(key, derivable);
-                derivable.connected$.react(() => cache.delete(key), { skipFirst: true, once: true });
+            // A cache miss means no other proxy is currently connected. If it is a one-time get, we can simply create the
+            // derivable, get the value and forget about it afterwards.
+            if (!this.connected) {
+                return derivableFactory(input).getState();
             }
-            return state;
+
+            // Apparently we are creating a Derivable that will be connected because of a reactor or autoCache. So
+            // we should now prepare the cache entry and make sure it gets removed on disconnect.
+            const inner = _internal.independentTracking(() => derivableFactory(input));
+            cache.set(key, { inner, outer: this });
+            // We know (from before) that `this.connected`, so we just have to wait until `connected` becomes false.
+            this.connected$.react((connected, stop) => {
+                if (!connected) {
+                    cache.delete(key);
+                    stop();
+                }
+            });
+
+            return inner.getState();
         },
-        set(newValue, key) {
-            const derivable = cache.get(key) || derivableFactory(key);
+        set(newValue, input, key) {
+            const derivable = cache.get(key)?.inner ?? derivableFactory(input);
             if (!isSettableDerivable(derivable)) {
                 throw _internal.augmentStack(new Error('Cached derivable is not settable'), derivable);
             }
@@ -82,18 +73,33 @@ export function derivableCache<K, V>(opts: DerivableCacheOptions<K, V>): Derivab
         },
     };
 
-    function derivableCache(key: Unwrappable<K>): SettableDerivable<V> {
-        if (isDerivable(key)) {
-            return key.flatMap(derivableCache);
+    function result(input: I) {
+        const cacheKey = mapKeys(input);
+        const cacheEntry = cache.get(cacheKey);
+        if (cacheEntry) {
+            return cacheEntry.outer;
         }
-        const cacheItem = cache.get(key);
-        if (cacheItem) {
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            return cacheItem[CACHED_PROXY]!;
-        }
-
-        return lens(descriptor, key);
+        const derivable = lens(descriptor, input, cacheKey);
+        delayedEviction && derivable.autoCache();
+        return derivable;
     }
+    result.observerCount = () => injectedDependency.observerCount;
+    result.connectedKeys = () => cache.keys();
+    result.connectedKeyCount = () => cache.size;
+    return result;
+}
 
-    return derivableCache;
+interface CacheItem<V> {
+    /**
+     * This is the derivable as created by the derivableFactory. It should be created only once per key while connected, but
+     * it will be created every time if not connected.
+     */
+    inner: Derivable<V>;
+
+    /**
+     * This is the returned derivable from the derivableCache when connected. Because we maintain a cache entry while connected
+     * we can maintain a reference to the returned derivable which would otherwise be a memory-leak. So during connection, the
+     * same derivable will be returned every time per key, otherwise a new instance (proxy) will be returned on each call.
+     */
+    outer: SettableDerivable<V>;
 }
