ba13768e98b556a010a3c257aec3d929bb626476
Paco van der Linden <pavadeli@gmail.com>
2023-06-21 11:30:42 +0200

    feat(sherlock)!: Support RxJS 7
    Note that this version might not work with previous versions of RxJS.

--- libs/sherlock/src/lib/reactor/rxjs-interop.ts
+++ libs/sherlock/src/lib/reactor/rxjs-interop.ts
@@ -45,14 +45,7 @@ declare global {
 }
 
 interface Subscribable<T> {
-    subscribe(observer?: PartialObserver<T>): Unsubscribable;
-    /** @deprecated Use an observer instead of a complete callback */
-    subscribe(next: null | undefined, error: null | undefined, complete: () => void): Unsubscribable;
-    /** @deprecated Use an observer instead of an error callback */
-    subscribe(next: null | undefined, error: (error: any) => void, complete?: () => void): Unsubscribable;
-    /** @deprecated Use an observer instead of a complete callback */
-    subscribe(next: (value: T) => void, error: null | undefined, complete: () => void): Unsubscribable;
-    subscribe(next?: (value: T) => void, error?: (error: any) => void, complete?: () => void): Unsubscribable;
+    subscribe(observer?: Partial<Observer<T>> | ((value: T) => void)): Unsubscribable;
 }
 
 interface Observer<T> {
