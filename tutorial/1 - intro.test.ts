89b4d83935bb31131bff2c59dd43b596f4a82143
Robin Kneepkens <robin@skunk.team>
2023-11-09 14:40:05 +0100

    Got test 1 in working order

--- tutorial/1 - intro.test.ts
+++ tutorial/1 - intro.test.ts
@@ -1,3 +1,5 @@
+import { atom } from '../libs/sherlock/src';
+
 /**
  * **Your Turn**
  * If you see this variable, you should do something about it. :-)
@@ -17,7 +19,9 @@ export const __YOUR_TURN__ = {} as any;
  * *Hint: most methods and functions are fairly well documented in jsDoc, which is easily accessed through TypeScript*
  */
 describe('intro', () => {
-    it('should be clear what to do next', () => {
+    it(`--- Welcome to the tutorial! ---
+    
+    Please look in \`./tutorial/1 - intro.ts\` to see what to do next.`, () => {
         // At the start of the spec, there will be some setup.
         let bool = false;
         // Sometimes including an expectation, to show the current state.
@@ -31,87 +35,83 @@ describe('intro', () => {
         bool = __YOUR_TURN__;
 
         // We use expectations like this to verify the result.
-        expect(`
---- Welcome to the tutorial! ---
-
-Please look in \`./tutorial/1 - intro.ts\` to see what to do next..
-        `).toBeFalsy();
+        expect(bool).toBeTrue();
     });
 });
 
 /**
  * Let's start with the `Derivable` basics.
  */
-// describe.skip('the basics', () => {
-//     /**
-//      * The `Atom` is the basic building block of `@politie/sherlock`.
-//      * It holds a value which you can `get()` and `set()`.
-//      */
-//     it('the `Atom`', () => {
-//         // An `Atom` can be created with the `atom()` function. The parameter of this function is used as the initial value of the `Atom`.
-//         const myValue$ = atom(1);
-//         // Variables containing `Atom`s or any other `Derivable` are usually postfixed with a `$` to indicate this. Hence `myValue$`.
+describe.skip('the basics', () => {
+    /**
+     * The `Atom` is the basic building block of `@politie/sherlock`.
+     * It holds a value which you can `get()` and `set()`.
+     */
+    it('the `Atom`', () => {
+        // An `Atom` can be created with the `atom()` function. The parameter of this function is used as the initial value of the `Atom`.
+        const myValue$ = atom(1);
+        // Variables containing `Atom`s or any other `Derivable` are usually postfixed with a `$` to indicate this. Hence `myValue$`.
 
-//         // The `.get()` method can be used to get the current value of the `Atom`.
-//         expect(myValue$.get()).to.equal(1);
+        // The `.get()` method can be used to get the current value of the `Atom`.
+        expect(myValue$.get()).toEqual(1);
 
-//         /**
-//          * **Your Turn**
-//          * Use the `.set(<newValue>)` method to change the value of the `Atom`.
-//          */
+        /**
+         * **Your Turn**
+         * Use the `.set(<newValue>)` method to change the value of the `Atom`.
+         */
 
-//         expect(myValue$.get()).to.equal(2);
-//     });
+        expect(myValue$.get()).toEqual(2);
+    });
 
-//     /**
-//      * The `Atom` is a `Derivable`. This means it can be used to create a derived value.
-//      * This derived value stays up to date with the original `Atom`.
-//      *
-//      * The easiest way to do this, is to call `.derive()` on another `Derivable`.
-//      * Let's try this.
-//      */
-//     it('the `Derivable`', () => {
-//         const myValue$ = atom(1);
-//         expect(myValue$.get()).to.equal(1);
+    /**
+     * The `Atom` is a `Derivable`. This means it can be used to create a derived value.
+     * This derived value stays up to date with the original `Atom`.
+     *
+     * The easiest way to do this, is to call `.derive()` on another `Derivable`.
+     * Let's try this.
+     */
+    it('the `Derivable`', () => {
+        const myValue$ = atom(1);
+        expect(myValue$.get()).toEqual(1);
 
-//         /**
-//          * **Your Turn**
-//          * We want to create a new `Derivable` that outputs the inverse of the original `Atom`.
-//          * Use `myValue$.derive(val => ...)` to create the `myInverse$` variable.
-//          */
-//         const myInverse$ = myValue$.derive(__YOUR_TURN__);
+        /**
+         * **Your Turn**
+         * We want to create a new `Derivable` that outputs the inverse of the original `Atom`.
+         * Use `myValue$.derive(val => ...)` to create the `myInverse$` variable.
+         */
+        const myInverse$ = myValue$.derive(__YOUR_TURN__);
 
-//         expect(myInverse$.get()).to.equal(-1);
+        expect(myInverse$.get()).toEqual(-1);
 
-//         // So if we set `myValue$` to -2:
-//         myValue$.set(-2);
-//         // `myInverse$` will change accordingly.
-//         expect(myInverse$.get()).to.equal(2);
-//     });
+        // So if we set `myValue$` to -2:
+        myValue$.set(-2);
+        // `myInverse$` will change accordingly.
+        expect(myInverse$.get()).toEqual(2);
+    });
 
-//     /**
-//      * Of course, `Derivable`s are not only meant to get, set and derive state.
-//      * You can also listen to the changes.
-//      *
-//      * This is done with the `.react()` method.
-//      * This method is given a `function` that is executed every time the value of the `Derivable` changes.
-//      */
-//     it('reacting to `Derivable`s', () => {
-//         const myCounter$ = atom(0);
+    /**
+     * Of course, `Derivable`s are not only meant to get, set and derive state.
+     * You can also listen to the changes.
+     *
+     * This is done with the `.react()` method.
+     * This method is given a `function` that is executed every time the value of the `Derivable` changes.
+     */
+    it('reacting to `Derivable`s', () => {
+        const myCounter$ = atom(0);
 
-//         let reacted = 0;
-//         /**
-//          * **Your Turn**
-//          * Now react to `myCounter$`. In every `react()`, increase the `reacted` variable by one.
-//          */
+        let reacted = 0;
+        /**
+         * **Your Turn**
+         * Now react to `myCounter$`. In every `react()`, increase the `reacted` variable by one.
+         */
 
-//         expect(reacted).to.equal(1); // `react()` will react immediately, more on that later.
+        expect(reacted).toEqual(1); // `react()` will react immediately, more on that later.
 
-//         // And then we set the `Atom` a couple of times to make the `Derivable` react.
-//         for (let i = 0; i <= 100; i++) {
-//             // Set the value of the `Atom`.
-//             myCounter$.set(i);
-//         }
-//         expect(reacted).to.equal(101);
-//     });
-// });
+        // And then we set the `Atom` a couple of times to make the `Derivable` react.
+        for (let i = 0; i <= 100; i++) {
+            // Set the value of the `Atom`.
+            myCounter$.set(i);
+        }
+        expect(reacted).toEqual(101);
+    });
+});
