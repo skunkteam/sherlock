dfc094fbe2a7e7a5e6896e571d9fe30ff322f736
Robin Kneepkens <robin@skunk.team>
2023-11-17 14:14:47 +0100

    Got tutorial 9 in working state
    Probably have to tweak it a bit more though

--- tutorial/9 - expert.test.ts
+++ tutorial/9 - expert.test.ts
@@ -1,7 +1,5 @@
-import { expect } from 'chai';
-import { SinonStub, spy, stub } from 'sinon';
-import { derivableCache } from '../libs/sherlock-utils/src';
-import { DerivableAtom, atom, derive } from '../libs/sherlock/src';
+import { DerivableAtom, atom, derive } from '@skunkteam/sherlock';
+import { derivableCache } from '@skunkteam/sherlock-utils';
 
 /**
  * **Your Turn**
@@ -16,7 +14,7 @@ describe.skip('expert', () => {
          * But what if a `Derivable` is used multiple times in another `Derivable`.
          */
         it('multiple executions', () => {
-            const hasDerived = spy();
+            const hasDerived = jest.fn();
 
             const myAtom$ = atom(true);
             const myFirstDerivation$ = myAtom$.derive(hasDerived);
@@ -28,7 +26,7 @@ describe.skip('expert', () => {
              * **Your Turn**
              * `hasDerived` is used in the first derivation. But has it been called at this point?
              */
-            expect(hasDerived).to.not.have.callCount(__YOUR_TURN__);
+            expect(hasDerived).not.toHaveBeenCalledTimes(__YOUR_TURN__);
 
             mySecondDerivation$.get();
 
@@ -37,7 +35,7 @@ describe.skip('expert', () => {
              * Now that we have gotten `mySecondDerivation$`, which calls `.get()` on the first multiple times.
              * How many times has the first `Derivable` actually executed it's derivation?
              */
-            expect(hasDerived).to.have.callCount(__YOUR_TURN__); // how many times?
+            expect(hasDerived).toHaveBeenCalledTimes(__YOUR_TURN__); // how many times?
         });
 
         /**
@@ -48,8 +46,8 @@ describe.skip('expert', () => {
          * So let's try the example above with this feature
          */
         it('autoCaching', async () => {
-            const firstHasDerived = spy();
-            const secondHasDerived = spy();
+            const firstHasDerived = jest.fn();
+            const secondHasDerived = jest.fn();
 
             /**
              * **Your Turn**
@@ -61,18 +59,25 @@ describe.skip('expert', () => {
                 secondHasDerived(myFirstDerivation$.get() + myFirstDerivation$.get()),
             );
 
-            expect(firstHasDerived, 'first before .get()').to.have.not.been.called;
-            expect(secondHasDerived, 'second before .get()').to.have.not.been.called;
+            // first before .get()
+            expect(firstHasDerived).not.toHaveBeenCalled();
+
+            // second before .get()
+            expect(secondHasDerived).not.toHaveBeenCalled();
 
             mySecondDerivation$.get();
 
-            expect(firstHasDerived, 'first after first .get()').to.have.been.calledOnce;
-            expect(secondHasDerived, 'second after first .get()').to.have.been.calledOnce;
+            // first after first .get()
+            expect(firstHasDerived).toHaveBeenCalledOnce();
+            // second after first .get()
+            expect(secondHasDerived).toHaveBeenCalledOnce();
 
             mySecondDerivation$.get();
 
-            expect(firstHasDerived, 'first after second .get()').to.have.been.calledOnce;
-            expect(secondHasDerived, 'second after second .get()').to.have.been.calledTwice;
+            // first after second .get()
+            expect(firstHasDerived).toHaveBeenCalledOnce();
+            // second after second .get()
+            expect(secondHasDerived).toHaveBeenCalledTimes(2);
 
             /**
              * Notice that the first `Derivable` has only been executed once, even though the second `Derivable` executed twice.
@@ -81,8 +86,8 @@ describe.skip('expert', () => {
 
             await new Promise(r => setTimeout(r, 1));
 
-            firstHasDerived.resetHistory();
-            secondHasDerived.resetHistory();
+            firstHasDerived.mockClear();
+            secondHasDerived.mockClear();
 
             mySecondDerivation$.get();
 
@@ -90,8 +95,10 @@ describe.skip('expert', () => {
              * **Your Turn**
              * Now what do you expect?
              */
-            expect(firstHasDerived, 'first after last .get()').to.have.callCount(__YOUR_TURN__); // How many times was it called?
-            expect(secondHasDerived, 'second after last .get()').to.have.callCount(__YOUR_TURN__); // How many times was it called?
+            // first after last .get()
+            expect(firstHasDerived).toHaveBeenCalledTimes(__YOUR_TURN__); // How many times was it called?
+            // second after last .get()
+            expect(secondHasDerived).toHaveBeenCalledTimes(__YOUR_TURN__); // How many times was it called?
         });
     });
 
@@ -108,12 +115,18 @@ describe.skip('expert', () => {
      */
     describe('`derivableCache`', () => {
         type Stocks = 'GOOGL' | 'MSFT' | 'APPL';
-        let stockPrice$: SinonStub<[Stocks], DerivableAtom<number>>;
-        beforeEach(() => (stockPrice$ = stub<[Stocks], DerivableAtom<number>>().callsFake(() => atom.unresolved())));
 
-        const reactSpy = spy();
-        beforeEach(() => reactSpy.resetHistory());
+        let stockPrice$: jest.Mock<DerivableAtom<number>, [Stocks], any>;
+        const reactSpy = jest.fn();
+
+        beforeEach(() => {
+            // By default the stock price retriever returns unresolved.
+            stockPrice$ = jest.fn(_ => atom.unresolved());
+            reactSpy.mockClear();
+        });
+
         function reactor(v: any) {
+            // TODO: add stopper function in type definition?
             reactSpy(v);
         }
 
@@ -122,8 +135,9 @@ describe.skip('expert', () => {
          * Any setup this `Derivable` does, will be executed every time.
          */
         it('multiple setups', () => {
-            // To not make things difficult with `unresolved` for this example, imagine we get a response synchronously
-            stockPrice$.returns(atom(1079.11));
+            // To not make things difficult with `unresolved`
+            // for this example, imagine we get a response synchronously
+            stockPrice$ = jest.fn(_ => atom(1079.11));
 
             const html$ = derive(
                 () => `
@@ -135,8 +149,8 @@ describe.skip('expert', () => {
             );
             html$.react(reactor);
 
-            expect(html$.connected).to.be.true;
-            expect(reactSpy).to.have.been.calledOnce;
+            expect(html$.connected).toEqual(true);
+            expect(reactSpy).toHaveBeenCalledOnce();
 
             /**
              * **Your Turn**
@@ -146,7 +160,7 @@ describe.skip('expert', () => {
              * But does that apply here?
              * How many times has the setup run, for the price `Derivable`.
              */
-            expect(stockPrice$).to.have.callCount(__YOUR_TURN__);
+            expect(stockPrice$).toHaveBeenCalledTimes(__YOUR_TURN__);
 
             /** Can you explain this behavior? */
         });
@@ -169,13 +183,13 @@ describe.skip('expert', () => {
                 price$.react(reactor);
 
                 // Because the stockPrice is still `unresolved` the reactor should not have emitted anything yet
-                expect(reactSpy).to.have.not.been.called;
+                expect(reactSpy).not.toHaveBeenCalled();
 
                 // Now let's increase the price
                 // First we have to get the atom that was given by the `stockPrice$` stub
-                const googlPrice$ = stockPrice$.firstCall.returnValue as DerivableAtom<number>;
+                const googlPrice$ = stockPrice$.mock.results[0].value as DerivableAtom<number>;
                 // Check if it is the right `Derivable`
-                expect(googlPrice$.connected).to.be.true;
+                expect(googlPrice$.connected).toEqual(true);
 
                 // Then we set the price
                 googlPrice$.set(1079.11);
@@ -184,11 +198,12 @@ describe.skip('expert', () => {
                  * **Your Turn**
                  * So the value was increased. What do you think happened?
                  */
-                expect(reactSpy).to.have.callCount(__YOUR_TURN__);
-                expect(reactSpy).to.have.been.calledWith(__YOUR_TURN__);
+                expect(reactSpy).toHaveBeenCalledTimes(__YOUR_TURN__);
+                expect(reactSpy).toHaveBeenCalledWith(__YOUR_TURN__, expect.toBeFunction());
+
                 // And how many times did the setup run?
-                expect(stockPrice$).to.have.callCount(__YOUR_TURN__);
-                expect(googlPrice$.connected).to.equal(__YOUR_TURN__);
+                expect(stockPrice$).toHaveBeenCalledTimes(__YOUR_TURN__);
+                expect(googlPrice$.connected).toEqual(__YOUR_TURN__);
 
                 /**
                  * Can you explain this behavior?
@@ -238,15 +253,13 @@ describe.skip('expert', () => {
                 prices$.react(reactor);
 
                 // Because we use `.value` instead of `.get()` the reactor should emit immediately, this time
-                expect(reactSpy)
-                    .to.have.been.calledOnce // But it should emit `undefined`
-                    .and.calledWithExactly([undefined]);
+                expect(reactSpy).toHaveBeenCalledExactlyOnceWith([undefined]); // But it should emit `undefined` // TODO: Might also need the expect.toBeFunction() generic.
 
                 // Now let's increase the price
                 // First we have to get the atom that was given by the `stockPrice$` stub
-                const googlPrice$ = stockPrice$.firstCall.returnValue;
+                const googlPrice$ = stockPrice$.mock.results[0].value as DerivableAtom<number>;
                 // Check if it is the right `Derivable`
-                expect(googlPrice$.connected).to.be.true;
+                expect(googlPrice$.connected).toBe(true);
 
                 // Then we set the price, as before
                 googlPrice$.set(1079.11);
@@ -255,23 +268,23 @@ describe.skip('expert', () => {
                  * **Your Turn**
                  * So the value was increased. What do you think happened now?
                  */
-                expect(reactSpy).to.have.callCount(__YOUR_TURN__);
-                expect(reactSpy).to.have.been.calledWith([__YOUR_TURN__]);
+                expect(reactSpy).toHaveBeenCalledTimes(__YOUR_TURN__);
+                expect(reactSpy).toHaveBeenCalledWith([__YOUR_TURN__], expect.toBeFunction());
 
                 /**
                  * So that worked, now let's try and add another company to the list
                  */
                 companies$.swap(current => [...current, 'APPL']);
 
-                expect(companies$.get()).to.deep.equal(['GOOGL', 'APPL']);
+                expect(companies$.get()).toEqual(['GOOGL', 'APPL']);
 
                 /**
                  * **Your Turn**
                  * With both 'GOOGL' and 'APPL' in the list, what do we expect as an output?
                  * We had a price for 'GOOGL', but not for 'APPL'...
                  */
-                expect(reactSpy).to.have.callCount(__YOUR_TURN__);
-                expect(reactSpy).to.have.been.calledWith([__YOUR_TURN__, __YOUR_TURN__]);
+                expect(reactSpy).toHaveBeenCalledTimes(__YOUR_TURN__);
+                expect(reactSpy).toHaveBeenCalledWith([__YOUR_TURN__, __YOUR_TURN__], expect.toBeFunction());
             });
         });
         /**
@@ -283,9 +296,7 @@ describe.skip('expert', () => {
              * `derivableCache` requires a `derivableFactory`, this specifies the setup for a given key.
              * We know the key, and what to do with it, so let's try it!
              */
-            const priceCache$ = derivableCache({
-                derivableFactory: (company: Stocks) => stockPrice$(company),
-            });
+            const priceCache$ = derivableCache((company: Stocks) => stockPrice$(company));
             /**
              * *Note that from this point forward we use `priceCache$` where we used to use `stockPrice$` directly*
              */
@@ -306,14 +317,16 @@ describe.skip('expert', () => {
 
                 html$.react(reactor);
 
-                expect(html$.connected).to.be.true;
-                expect(reactSpy).to.have.been.calledOnce;
+                expect(html$.connected).toEqual(true);
+                expect(reactSpy).toHaveBeenCalledOnce();
                 // Convenience function to return the first argument of the last call to the reactor
                 function lastEmittedHTMLs() {
-                    return reactSpy.lastCall.args[0];
+                    // TODO: lastCall may be undefined, but it might be fine here.
+                    return reactSpy.mock.lastCall[0];
                 }
+
                 // The last call, should have the array of HTML's as first argument
-                expect(lastEmittedHTMLs()[0]).to.contain('$ unknown');
+                expect(lastEmittedHTMLs()[0]).toContain('$ unknown');
 
                 /**
                  * **Your Turn**
@@ -322,20 +335,20 @@ describe.skip('expert', () => {
                  *
                  * Has anything changed, by using the `derivableCache`?
                  */
-                expect(stockPrice$).to.have.callCount(__YOUR_TURN__);
+                expect(stockPrice$).toHaveBeenCalledTimes(__YOUR_TURN__);
 
                 // Now let's resolve the price
-                stockPrice$.firstCall.returnValue.set(1079.11);
+                stockPrice$.mock.results[0].value.set(1079.11);
 
                 /**
                  * **Your Turn**
                  * Last time this caused the setup to run again, resolving to `unresolved` yet again.
                  * What happens this time? Has the setup run again?
                  */
-                expect(stockPrice$).to.have.callCount(__YOUR_TURN__);
+                expect(stockPrice$).toHaveBeenCalledTimes(__YOUR_TURN__);
                 // Ok, but did it update the HTML?
-                expect(reactSpy).to.have.callCount(__YOUR_TURN__);
-                expect(lastEmittedHTMLs()[0]).to.contain(__YOUR_TURN__);
+                expect(reactSpy).toHaveBeenCalledTimes(__YOUR_TURN__);
+                expect(lastEmittedHTMLs()[0]).toContain(__YOUR_TURN__);
 
                 // Last chance, what if we add a company
                 companies$.swap(current => [...current, 'APPL']);
@@ -345,12 +358,12 @@ describe.skip('expert', () => {
                  * Now the `stockPrice$` function should have at least run again for 'APPL'.
                  * But did it calculate 'GOOGL' again too?
                  */
-                expect(stockPrice$).to.have.callCount(__YOUR_TURN__);
-                expect(reactSpy).to.have.callCount(__YOUR_TURN__);
+                expect(stockPrice$).toHaveBeenCalledTimes(__YOUR_TURN__);
+                expect(reactSpy).toHaveBeenCalledTimes(__YOUR_TURN__);
                 // The first should be 'GOOGL'
-                expect(lastEmittedHTMLs()[0]).to.contain(__YOUR_TURN__);
+                expect(lastEmittedHTMLs()[0]).toContain(__YOUR_TURN__);
                 // The first should be 'APPL'
-                expect(lastEmittedHTMLs()[1]).to.contain(__YOUR_TURN__);
+                expect(lastEmittedHTMLs()[1]).toContain(__YOUR_TURN__);
             });
         });
     });
