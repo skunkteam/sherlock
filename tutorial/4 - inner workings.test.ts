0883841db4b5c1899965d9b2dca82aa709dbbd5b
Robin Kneepkens <robin@skunk.team>
2023-11-21 16:27:58 +0100

    Finalized tutorial 4

--- tutorial/4 - inner workings.test.ts
+++ tutorial/4 - inner workings.test.ts
@@ -2,18 +2,20 @@ import { atom } from '@skunkteam/sherlock';
 import { Seq } from 'immutable';
 
 /**
- * **Your Turn**
+ * ** Your Turn **
+ *
  * If you see this variable, you should do something about it. :-)
  */
 export const __YOUR_TURN__ = {} as any;
 
 /**
- * Time to dive a bit deeper into the inner workings of `@politie/sherlock`.
+ * Time to dive a bit deeper into the inner workings of `@skunkteam/sherlock`.
  */
 describe.skip('inner workings', () => {
+describe('inner workings', () => {
     /**
-     * What if there is a derivation that reads from one of two `Derivable`s dynamically?
-     * Will both of those `Derivable`s be tracked for changes?
+     * What if there is a derivation that reads from one of two `Derivable`s
+     * dynamically? Will both of those `Derivable`s be tracked for changes?
      */
     it('dynamic/inactive dependencies', () => {
         const switch$ = atom(true);
@@ -23,23 +25,23 @@ describe.skip('inner workings', () => {
         const reacted = jest.fn();
 
         switch$
-            // This `.derive()` is the one we are testing when true, it will return the `number` otherwise the `string`
+            // This `.derive()` is the one we are testing when true, it will
+            // return the `number` otherwise the `string`
             .derive(s => (s ? number$.get() : string$.get()))
-            // Note: reacted is being called as reacted(value, stop),
-            // where stop is a function used to stop the reaction from within reacted.
             .react(reacted);
 
         // The first time should not surprise anyone, the derivation
         // was called and returned the right result.
-        // Again note here the second expectation (.toBeFunction()) to
-        // catch the stop function that was part of the .react() signature.
         expect(reacted).toHaveBeenCalledExactlyOnceWith(1, expect.toBeFunction());
+        // Note here the second expectation `.toBeFunction()` to
+        // catch the stop function that was part of the .react() signature.
 
         // `switch$` is still set to true (number)
         string$.set('two');
 
         /**
-         * **Your Turn**
+         * ** Your Turn **
+         *
          * What do you expect?
          */
         expect(reacted).toHaveBeenCalledTimes(__YOUR_TURN__);
@@ -49,21 +51,25 @@ describe.skip('inner workings', () => {
         number$.set(2);
 
         /**
-         * **Your Turn**
+         * ** Your Turn **
+         *
          * What do you expect?
          */
         expect(reacted).toHaveBeenCalledTimes(__YOUR_TURN__);
         expect(reacted).toHaveBeenLastCalledWith(__YOUR_TURN__, expect.toBeFunction());
 
-        // Now let's reset the spy, so callCount should be 0 again.
+        // Now let's reset the mock function, so the call count should
+        // be 0 again.
         reacted.mockClear();
+        expect(reacted).toHaveBeenCalledTimes(0);
 
         // `switch$` is set to false (string)
         switch$.set(false);
         number$.set(3);
 
         /**
-         * **Your Turn**
+         * ** Your Turn **
+         *
          * What do you expect now?
          */
         expect(reacted).toHaveBeenCalledTimes(__YOUR_TURN__);
@@ -71,7 +77,9 @@ describe.skip('inner workings', () => {
     });
 
     /**
-     * One thing to know about `Derivable`s is that derivations are not executed, until someone asks.
+     * One thing to know about `Derivable`s is that derivations are not
+     * executed, until someone asks.
+     *
      * So let's test this.
      */
     it('lazy execution', () => {
@@ -81,32 +89,47 @@ describe.skip('inner workings', () => {
         const myDerivation$ = myAtom$.derive(hasDerived);
 
         /**
-         * **Your Turn**
-         * We have created a new `Derivable` by deriving the `Atom`. But have not called `.get()` on that new `Derivable`.
-         * Do you think the `hasDerived` function has been called? And how many times?
-         * *Hint: you can use sinonChai's `.to.have.been.called`/`.to.have.been.calledOnce`/`to.have.callCount(...)`/etc..*
+         * ** Your Turn **
+         *
+         * We have created a new `Derivable` by deriving the `Atom`. But have
+         * not called `.get()` on that new `Derivable`.
+         *
+         * How many times do you think the `hasDerived` function has been
+         * called? 0 is also an option of course.
          */
-        expect(hasDerived).toHaveBeenCalledTimes(__YOUR_TURN__); // Well, what do you expect?
+
+        // Well, what do you expect?
+        expect(hasDerived).toHaveBeenCalledTimes(__YOUR_TURN__);
 
         myDerivation$.get();
 
-        expect(hasDerived).toHaveBeenCalledTimes(__YOUR_TURN__); // And after a `.get()`?
+        // And after a `.get()`?
+        expect(hasDerived).toHaveBeenCalledTimes(__YOUR_TURN__);
 
         myDerivation$.get();
 
-        expect(hasDerived).toHaveBeenCalledTimes(__YOUR_TURN__); // And after the second `.get()`? Is there an extra call?
+        // And after the second `.get()`? Is there an extra call?
+        expect(hasDerived).toHaveBeenCalledTimes(__YOUR_TURN__);
 
         /**
          * The state of any `Derivable` can change at any moment.
-         * But you don't want to keep a record of the state and changes to a `Derivable` that no one is listening to.
-         * That's why a `Derivable` has to recalculate it's internal state every time `.get()` is called.
+         *
+         * But you don't want to keep a record of the state and changes to a
+         * `Derivable` that no one is listening to.
+         *
+         * That's why a `Derivable` has to recalculate it's internal state every
+         * time `.get()` is called.
          */
     });
 
     /**
      * So what if the `Derivable` is reacting?
+     *
      * When a `Derivable` is reacting, the current state is known.
-     * And since changes are derived/reacted to synchronously, the state is always up to date.
+     *
+     * And since changes are derived/reacted to synchronously, the state is
+     * always up to date.
+     *
      * So a `.get()` should not have to be calculated.
      */
     it('while reacting', () => {
@@ -121,9 +144,9 @@ describe.skip('inner workings', () => {
         const stopper = myDerivation$.react(() => '');
 
         /**
-         * **Your Turn**
+         * ** Your Turn **
+         *
          * Ok, it's your turn to complete the expectations.
-         * *Hint: you can use `.calledOnce`/`.calledTwice` etc or `.callCount()`*
          */
         expect(hasDerived).toHaveBeenCalledTimes(__YOUR_TURN__);
 
@@ -148,8 +171,11 @@ describe.skip('inner workings', () => {
         expect(hasDerived).toHaveBeenCalledTimes(__YOUR_TURN__);
 
         /**
-         * Since the `.react()` already listens to the value(changes) there is no need to recalculate whenever a `.get()` is called.
-         * But when the reactor has stopped, the derivation has to be calculated again.
+         * Since the `.react()` already listens to the value(changes) there is
+         * no need to recalculate whenever a `.get()` is called.
+         *
+         * But when the reactor has stopped, the derivation has to be calculated
+         * again.
          */
     });
 
@@ -163,7 +189,7 @@ describe.skip('inner workings', () => {
 
         const myAtom$ = atom(1);
         const first$ = myAtom$.derive(i => {
-            first(i); // Call the spy, to let it know we were here
+            first(i); // Call the mock function, to let it know we were here
             return i > 2;
         });
         const second$ = first$.derive(second);
@@ -179,10 +205,13 @@ describe.skip('inner workings', () => {
         expect(second).toHaveBeenCalledOnce();
 
         /**
-         * **Your Turn**
+         * ** Your Turn **
+         *
          * But what to expect now?
          */
-        myAtom$.set(1); // Note that this is the same value as it was initialized with
+
+        // Note that this is the same value as it was initialized with
+        myAtom$.set(1);
 
         expect(first).toHaveBeenCalledTimes(__YOUR_TURN__);
         expect(second).toHaveBeenCalledTimes(__YOUR_TURN__);
@@ -205,18 +234,23 @@ describe.skip('inner workings', () => {
         /**
          * Can you explain the behavior above?
          *
-         * It is why we say that `@politie/sherlock` deals with reactive state and not events (as RxJS does for example).
-         * Events can be very useful, but when data is involved, you are probably only interested in value changes.
-         * So these changes can and need to be cached and deduplicated.
+         * It is why we say that `@skunkteam/sherlock` deals with reactive state
+         * and not events (as RxJS does for example).
+         *
+         * Events can be very useful, but when data is involved, you are
+         * probably only interested in value changes. So these changes can and
+         * need to be cached and deduplicated.
          */
     });
 
     /**
-     * So if the new value of a `Derivable` is equal to the old, it won't propagate a new event.
-     * But what does it mean to be equal in a `Derivable`.
+     * So if the new value of a `Derivable` is equal to the old, it won't
+     * propagate a new event. But what does it mean to be equal in a
+     * `Derivable`?
      *
-     * Strict `===` equality would mean that `NaN` and `NaN` would not even be equal.
-     * `Object.is()` equality would be better, but would mean that structurally equal objects could be different.
+     * Strict `===` equality would mean that `NaN` and `NaN` would not even be
+     * equal. `Object.is()` equality would be better, but would mean that
+     * structurally equal objects could be different.
      */
     it('equality', () => {
         const atom$ = atom<unknown>({});
@@ -227,13 +261,17 @@ describe.skip('inner workings', () => {
         atom$.set({});
 
         /**
-         * **Your Turn**
-         * The `Atom` is set with exactly the same object as before. Will the `.react()` fire?
+         * ** Your Turn **
+         *
+         * The `Atom` is set with exactly the same object as before. Will the
+         * `.react()` fire?
          */
         expect(hasReacted).toHaveBeenCalledTimes(__YOUR_TURN__);
 
         /**
-         * But what if you use an object, that can be easily compared through a library like `ImmutableJS`
+         * But what if you use an object, that can be easily compared through a
+         * library like `ImmutableJS`?
+         *
          * Let's try an `Immutable.Seq`
          */
         atom$.set(Seq.Indexed.of(1, 2, 3));
@@ -243,75 +281,31 @@ describe.skip('inner workings', () => {
 
         atom$.set(Seq.Indexed.of(1, 2, 3));
         /**
-         * **Your Turn**
+         * ** Your Turn **
+         *
          * Do you think the `.react()` fired with this new value?
          */
-        expect(hasReacted).toHaveBeenCalledTimes(__YOUR_TURN__);
+        expect(hasReacted).toHaveBeenCalledTimes(0);
 
         atom$.set(Seq.Indexed.of(1, 2));
 
         /**
-         * **Your Turn**
+         * ** Your Turn **
+         *
          * And now?
          */
-        expect(hasReacted).toHaveBeenCalledTimes(__YOUR_TURN__);
+        expect(hasReacted).toHaveBeenCalledTimes(1);
 
         /**
-         * In `@politie/sherlock` equality is a bit complex.
-         * First we check `Object.is()` equality, if that is true, it is the same, you can't deny that.
+         * In `@skunkteam/sherlock` equality is a bit complex:
+         *
+         * First we check `Object.is()` equality, if that is true, it is the
+         * same, you can't deny that.
+         *
          * After that it is pluggable. It can be anything you want.
-         * By default we try to use `.equals()`, to support libraries like `ImmutableJS`.
+         *
+         * By default we try to use `.equals()`, to support libraries like
+         * `ImmutableJS`.
          */
-    });
-
-    /**
-     * What if there is a derivation that reads from one of two `Derivable`s dynamically?
-     * Will both of those `Derivable`s be tracked for changes?
-     */
-    it('dynamic/inactive dependencies', () => {
-        const switch$ = atom(true);
-        const number$ = atom(1);
-        const string$ = atom('one');
-
-        const reacted = jest.fn();
-
-        switch$
-            // This `.derive()` is the one we are testing when true, it will return the `number` otherwise the `string`
-            .derive(s => (s ? number$.get() : string$.get()))
-            .react(reacted);
-
-        // The first time should not surprise anyone, the derivation was called and returned the right result
-        expect(reacted).toHaveBeenCalledExactlyOnceWith(1, expect.toBeFunction());
-
-        // `switch$` is still set to true (number)
-        string$.set('two');
-
-        /**
-         * **Your Turn**
-         * What do you expect?
-         */
-        expect(reacted).toHaveBeenCalledTimes(__YOUR_TURN__);
-
-        // `switch$` is still set to true (number)
-        number$.set(2);
-
-        /**
-         * **Your Turn**
-         * What do you expect?
-         */
-        expect(reacted).toHaveBeenCalledTimes(__YOUR_TURN__);
-
-        // Now let's reset the spy, so callCount should be 0 again.
-        reacted.mockClear();
-
-        // `switch$` is set to false (string)
-        switch$.set(false);
-        number$.set(3);
-
-        /**
-         * **Your Turn**
-         * What do you expect now?
-         */
-        expect(reacted).toHaveBeenCalledTimes(__YOUR_TURN__);
     });
 });
